763b4e31769d7568cee04773a4a8f6fe12fc7d21
Nicole Provan <provanni@sheridanc.on.ca>
2018-09-07 12:02:49 -0400

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi people</h1>
+    <p>My name is Nicole Provan. I am super excited to participate in the online
+    JAMstack Hackathon this October! This will be my very first Hackathon and I
+    am looking forward to the challenges.</p>
+    <p>Now go build something great.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
